f077c67b31b50a4d204ba364dd2720095feb7ae5
Sysreqx <sysreqx@gmail.com>
2018-12-04 20:39:05 +0600

    create SearchBArElement

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -6,3 +6,16 @@ sortByOptions {
 	'Highest Rated': 'rating',
 	'Most Reviewed': 'review_count'
 }
+
+/*
+Object.keys()
+Returns an array containing the names of all of the given object's own enumerable string properties.*/
+class SearchBar extends React.Component {
+	renderSortByOptions() {
+		return Object.keys(sortByOptions).map(sortByOption => {
+			let sortByOptionValue = sortByOptions[sortByOption];
+			return <li key={sortByOptionValue}>{sortByOption}</li>;
+		});
+
+	}
+}
