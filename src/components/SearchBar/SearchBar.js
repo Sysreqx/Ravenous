b359e06a44351f3554cd763245429d4767bd7015
Sysreqx <sysreqx@gmail.com>
2018-12-04 21:23:56 +0600

    errors removing part I

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './SearchBar.css';
 
-sortByOptions {
+const sortByOptions = {
 	'Best Match': 'best_match',
 	'Highest Rated': 'rating',
 	'Most Reviewed': 'review_count'
@@ -19,20 +19,22 @@ class SearchBar extends React.Component {
 	}
 
 	render() {
-		<div className="SearchBar">
-			<div className="SearchBar-sort-options">
-				<ul>
-					{this.renderSortByOptions()}
-				</ul>
+		return (
+			<div className="SearchBar">
+				<div className="SearchBar-sort-options">
+					<ul>
+						{this.renderSortByOptions()}
+					</ul>
+				</div>
+				<div className="SearchBar-fields">
+					<input placeholder="Search Businesses" />
+					<input placeholder="Where?" />
+				</div>
+				<div className="SearchBar-submit">
+					<a>Let's Go</a>
+				</div>
 			</div>
-			<div className="SearchBar-fields">
-				<input placeholder="Search Businesses" />
-				<input placeholder="Where?" />
-			</div>
-			<div className="SearchBar-submit">
-				<a>Let's Go</a>
-			</div>
-		</div>
+		)
 	}
 };
 
