5b9780fdeb928bbf374f0365f947017c0a0c1867
Sysreqx <sysreqx@gmail.com>
2018-12-04 21:05:17 +0600

    chto-to da namutil

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -5,7 +5,7 @@ sortByOptions {
 	'Best Match': 'best_match',
 	'Highest Rated': 'rating',
 	'Most Reviewed': 'review_count'
-}
+};
 
 /*
 Object.keys()
@@ -16,6 +16,24 @@ class SearchBar extends React.Component {
 			let sortByOptionValue = sortByOptions[sortByOption];
 			return <li key={sortByOptionValue}>{sortByOption}</li>;
 		});
-
 	}
-}
+
+	render() {
+		<div className="SearchBar">
+			<div className="SearchBar-sort-options">
+				<ul>
+					{this.renderSortByOptions()}
+				</ul>
+			</div>
+			<div className="SearchBar-fields">
+				<input placeholder="Search Businesses" />
+				<input placeholder="Where?" />
+			</div>
+			<div className="SearchBar-submit">
+				<a>Let's Go</a>
+			</div>
+		</div>
+	}
+};
+
+export default SearchBar;
