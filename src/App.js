c895b84a667e1aff87d944a22d9432177460193a
Sysreqx <sysreqx@gmail.com>
2018-12-08 18:09:07 +0600

    visual feedback for the sorting options at the top of the search bar

--- src/App.js
+++ src/App.js
@@ -6,29 +6,33 @@ import BusinessList from './components/BusinessList/BusinessList.js';
 import SearchBar from './components/SearchBar/SearchBar.js';
 
 var business = {
-	imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-	name: 'MarginOtto Pizzeria',
-	address: '1010 Paddington Way',
-	city: 'Flavortown',
-	state: 'NY',
-	zipCode: '10101',
-	category: 'Italian',
-	rating: 4.5,
-	reviewCount: 90
+    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
 };
 
 var businesses = [business, business, business, business, business, business];
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
+	searchYelp(term, location, sortBy) {
+		console.log(`${term}, ${location}, ${sortBy}`)
+	}
+
+    render() {
+        return (
+            <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
+        <SearchBar searchYelp={this.searchYelp} />
         <BusinessList businesses={businesses} />
       </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
